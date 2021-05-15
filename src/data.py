c6addbb2e58a4c5bec9841d691de1ad87104dd42
bwconrad <benwconrad@gmail.com>
2021-05-15 08:26:59 +0200

    init commit

---
+++ src/data.py
@@ -0,0 +1,119 @@
+import pytorch_lightning as pl
+from pytorch_lightning.trainer.supporters import CombinedLoader
+import torch
+import torch.utils.data as data
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+import os
+from typing import Callable
+
+
+class ContentStyleDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        content_path: str = "data/coco/",
+        style_path: str = "data/wikiart/",
+        resize_size: int = 512,
+        crop_size: int = 256,
+        batch_size: int = 8,
+        workers: int = 4,
+    ):
+        """Content and style image data module
+
+        Args:
+            content_path: Path to content images directory
+            style_path: Path to style images directory
+            resize_size: Size of resize transformation
+            crop_size: Size of random crop transformation
+            batch_size: Number of batch samples
+            workers: Number of data loader workers
+        """
+        super().__init__()
+        self.content_path = content_path
+        self.style_path = style_path
+        self.batch_size = batch_size
+        self.workers = workers
+
+        self.transforms = transforms.Compose(
+            [
+                transforms.Resize(resize_size),
+                transforms.RandomCrop(crop_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+            ]
+        )
+
+    def setup(self, stage="fit"):
+        if stage == "fit":
+            # Load all content and style images
+            content_all = SimpleDataset(self.content_path, self.transforms)
+            style_all = SimpleDataset(self.style_path, self.transforms)
+
+            # Split into train/val
+            self.content_train, self.content_val = data.random_split(
+                content_all, [len(content_all) - 50, 50]
+            )
+            self.style_train, self.style_val = data.random_split(
+                style_all, [len(style_all) - 50, 50]
+            )
+
+    def train_dataloader(self):
+        return {
+            "content": DataLoader(
+                self.content_train,
+                batch_size=self.batch_size,
+                shuffle=True,
+                num_workers=self.workers,
+                pin_memory=True,
+            ),
+            "style": DataLoader(
+                self.style_train,
+                batch_size=self.batch_size,
+                shuffle=True,
+                num_workers=self.workers,
+                pin_memory=True,
+            ),
+        }
+
+    def val_dataloader(self):
+        loaders = {
+            "content": DataLoader(
+                self.content_val,
+                batch_size=1,
+                shuffle=False,
+                num_workers=self.workers,
+                pin_memory=True,
+            ),
+            "style": DataLoader(
+                self.style_val,
+                batch_size=1,
+                shuffle=False,
+                num_workers=self.workers,
+                pin_memory=True,
+            ),
+        }
+
+        return CombinedLoader(loaders, "max_size_cycle")
+
+
+class SimpleDataset(data.Dataset):
+    def __init__(self, root: str, transforms: Callable):
+        """Image dataset from directory
+
+        Args:
+            root: Path to directory
+            transforms: Image augmentations
+        """
+        super().__init__()
+        self.root = root
+        self.paths = os.listdir(root)
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.root, self.paths[index])).convert("RGB")
+        img = self.transforms(img)
+        return img
+
+    def __len__(self):
+        return len(self.paths)
---
+++ src/model.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from torchvision.utils import make_grid
+from torch.optim import lr_scheduler, Adam
+from collections import OrderedDict
+
+from .network import VGGEncoder, AdaIn, Decoder
+
+
+class ASTModel(pl.LightningModule):
+    def __init__(
+        self, lr: float = 1e-4, weight_content: float = 1.0, weight_style: float = 1.0
+    ):
+        """Arbitrary style transfer model
+
+        Args:
+            lr: Learning rate
+            weight_content: weight for content loss
+            weight_style: weight for style loss
+        """
+        super().__init__()
+        self.save_hyperparameters()
+
+        # Hyperparameters
+        self.lr = lr
+        self.weight_content = weight_content
+        self.weight_style = weight_style
+
+        # Modules
+        self.encoder = VGGEncoder()
+        self.ada_in = AdaIn()
+        self.decoder = Decoder()
+
+    def forward(self, c, s):
+        # Pass content and style images through encoder
+        f_c = self.encoder(c)
+        f_s = self.encoder(s, return_all=True)
+
+        # Apply AdaIn
+        t = self.ada_in(f_c, f_s[-1])
+
+        # Decode back
+        g_t = self.decoder(t)
+
+        return g_t, t, f_c, f_s
+
+    def content_loss(self, pred, target):
+        assert pred.size() == target.size()
+        return F.mse_loss(pred, target)
+
+    def style_loss(self, pred, target):
+        assert len(pred) == len(target)
+        loss = 0
+        for p, t in zip(pred, target):
+            assert p.size() == t.size()
+            p_mean, p_std = self.get_mean_std(p)
+            t_mean, t_std = self.get_mean_std(t)
+            loss += F.mse_loss(p_mean, t_mean) + F.mse_loss(p_std, t_std)
+        return loss
+
+    def get_mean_std(self, x):
+        mean = torch.mean(x, axis=(2, 3), keepdim=True)
+        std = torch.std(x, dim=(2, 3), keepdim=True) + 1e-6
+        return mean, std
+
+    def training_step(self, batch, batch_idx):
+        img_c = batch["content"]
+        img_s = batch["style"]
+
+        # Pass through model
+        g_t, t, f_c, f_s = self(img_c, img_s)
+
+        # Calculate loss
+        f_g_t = self.encoder(g_t, return_all=True)
+        loss_c = self.content_loss(f_g_t[-1], t)
+        loss_s = self.style_loss(f_g_t, f_s)
+        loss = self.weight_content * loss_c + self.weight_style * loss_s
+
+        self.log("train_loss", loss)
+        self.log("train_content_loss", loss_c, prog_bar=True)
+        self.log("train_style_loss", loss_s, prog_bar=True)
+        self.log("lr", self.trainer.optimizers[0].param_groups[0]["lr"], prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        img_c = batch["content"]
+        img_s = batch["style"]
+
+        # Pass through model
+        g_t, t, f_c, f_s = self(img_c, img_s)
+
+        # Calculate loss
+        f_g_t = self.encoder(g_t, return_all=True)
+        loss_c = self.content_loss(f_g_t[-1], t)
+        loss_s = self.style_loss(f_g_t, f_s)
+        loss = self.weight_content * loss_c + self.weight_style * loss_s
+
+        self.log("val_loss", loss)
+        self.log("val_content_loss", loss_c)
+        self.log("val_style_loss", loss_s)
+
+        return OrderedDict({"val_sample": torch.stack([img_c[0], img_s[0], g_t[0]])})
+
+    def validation_epoch_end(self, outputs):
+        imgs = torch.cat([x["val_sample"] for x in outputs], 0)
+        grid = make_grid(imgs, nrow=3)
+        self.logger.experiment.add_image("val_samples", grid, self.global_step)
+
+    def configure_optimizers(self):
+        optimizer = Adam(
+            self.decoder.parameters(),
+            lr=self.lr,
+        )
+        scheduler = {
+            "scheduler": lr_scheduler.CosineAnnealingLR(
+                optimizer,
+                T_max=self.trainer.max_steps,
+            ),
+            "interval": "step",
+        }
+        return [optimizer], [scheduler]
